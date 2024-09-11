369111438da835695d9a1481e40e0dd877849512
JoaoR090 <jrzr432@gmail.com>
2024-09-11 18:47:37 -0300

    Add files via upload

---
+++ javascript/dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        nome: 'Pânico',
+        sinopse: 'Um grupo de jovens enfrenta um assassino mascarado que testa seus conhecimentos sobre filmes de terror. A pequena cidade de Woodsboro nunca mais será a mesma.',
+        mais_informacoes : "https://pt.wikipedia.org/wiki/Scream_(1996)"
+    },
+    {
+        nome: 'It - a coisa',
+        sinopse: 'Um grupo de crianças se une para investigar o misterioso desaparecimento de vários jovens em sua cidade. Eles descobrem que o culpado é Pennywise, um palhaço cruel que se alimenta de seus medos e cuja violência teve origem há vários séculos.',
+        mais_informacoes : "https://pt.wikipedia.org/wiki/It_(2017)"
+    },
+    {
+        nome: 'Annabelle',
+        sinopse: 'John Form acha que encontrou o presente ideal para sua esposa grávida, uma boneca vintage. No entanto, a alegria do casal não dura muito. Uma noite terrível, membros de uma seita satânica invadem a casa do casal em um ataque violento. Ao tentarem invocar um demônio, eles mancham a boneca de sangue, tornando-a receptora de uma entidade do mal.',
+        mais_informacoes : "https://pt.wikipedia.org/wiki/Annabelle_(filme))"
+    },
+    {
+        nome: 'A Bruxa',
+        sinopse: 'Em uma Nova Inglaterra puritana, uma família colonizadora enfrenta forças sinistras que ameaçam sua sobrevivência após se isolarem em uma floresta remota.',
+        mais_informacoes: "https://pt.wikipedia.org/wiki/The_Witch_(2015)"
+    },
+    {
+        nome: 'Corra!',
+        sinopse: 'Um jovem negro visita a família de sua namorada branca em uma rica cidade rural e começa a desconfiar de que algo sinistro está acontecendo.',
+        mais_informacoes: "https://pt.wikipedia.org/wiki/Get_Out_(film)"
+    },
+    {
+        nome: 'Um Lugar Silencioso',
+        sinopse: 'Em uma fazenda nos Estados Unidos, uma família do Meio-Oeste é perseguida por uma entidade fantasmagórica assustadora. Para se protegerem, eles devem permanecer em silêncio absoluto, a qualquer custo, pois o perigo é ativado pela percepção do som.',
+        mais_informacoes: "https://pt.wikipedia.org/wiki/A_Quiet_Place"
+    }
+]
